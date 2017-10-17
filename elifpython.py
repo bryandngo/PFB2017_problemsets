bace693caf5ae7d523f87a939aa70be2d1acd4f6
admin <admin@pfb16.cshl.edu>
2017-10-17 16:23:39 -0400

    elifpython.py

---
+++ elifpython.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+count = 20 
+
+if count < 0:
+	message = "is less than 0"
+	print(count, message)
+
+elif count < 20: 
+	message = "is less than 20"
+	print(count, message)
+
+elif count > 20: 
+	message = "is greater than 20"
+	print(count, message) 
+
+elif count != 20:
+	message = "NOT TRUE"
+	print(count, message)
+
+else:
+	message = "TRUE"
+	print(count, message)
+
+ 
+ 
