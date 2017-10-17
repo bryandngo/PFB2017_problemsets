0cd9beb73a56aaa0c4af072feac3c2585b20a0d0
admin <admin@pfb16.cshl.edu>
2017-10-17 11:55:32 -0400

    python excerise unit 1

---
+++ pythonscript2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import sys		#import sys module 
+
+print (sys.argv[1])
+print (sys.argv[2])
+print (sys.argv[3])
+print (sys.argv[4])
---
+++ pythonscript_favorites.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+print ("Bryan") 	#print my name
+print ("Blue") 		#print my favorite color
+print ("Tennis") 	#print my favorite activity
+print ("Panda Bear")	#print my favorite animal 
+
+
